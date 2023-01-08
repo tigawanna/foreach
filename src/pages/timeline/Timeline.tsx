05d1337062b68d9c4463cd58958e5a0158312167
tigawanna <denniskinuthiaw@gmail.com>
2023-01-08 10:20:37 +0300

    added posts list

--- src/pages/timeline/Timeline.tsx
+++ src/pages/timeline/Timeline.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { PBUser } from '../../utils/types/types';
-import dayjs from 'dayjs';
-import { pb_url } from './../../utils/env';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer'
 import { useInfiniteCustom } from './../../utils/hooks/useInfiniteCustom';
+import { QueryStateWrapper } from './../../shared/wrappers/QueryStateWrapper';
+import { FaPlus } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { TheIcon } from '../../shared/wrappers/TheIcon';
 interface TimelineProps {
     user: PBUser
 }
@@ -27,9 +29,9 @@ export interface CustomPostType {
 }
 
 export const Timeline: React.FC<TimelineProps> = ({user}) => {
+const { ref, inView } = useInView()
 
-
-const customPostsQuery = useInfiniteCustom<CustomPostType>('custom=posts',user,{
+const customPostsQuery = useInfiniteCustom<CustomPostType>('custom-posts',user,{
     getNextPageParam: (lastPage, allPages) => {
         // console.log("last page ==== ",lastPage,allPages)
         if (lastPage && lastPage[lastPage.length - 1]) {
@@ -41,14 +43,49 @@ const customPostsQuery = useInfiniteCustom<CustomPostType>('custom=posts',user,{
         return;
     }
 })
+React.useEffect(() => {
+        if (inView) {
+            customPostsQuery.fetchNextPage()
+        }
+}, [inView])
 
-  console.log("custom query === ",customPostsQuery.data)
-
+const data = customPostsQuery.data
+console.log("custom query === ",data)
 return (
- <div className='w-full h-full flex flex-col items-center justify-center'>
-<div className='text-4xl font-bold font-serif'>
-    timeline
-</div>
- </div>
+    <QueryStateWrapper query={customPostsQuery}>
+
+        <div className='w-full min-h-full flex flex-col gap-2 items-center justify-center '>
+            <div className='w-[95%] h-full flex flex-col items-center justify-center gap-2 py-2'>
+                {data?.pages.map((page) => {
+                    // console.log("page=== ",page)
+                    return page.map((item) => {
+                        return <PostsCard item={item} key={item.post_id} user={user} />
+                    })
+                  
+                })
+                }
+            </div>
+
+
+            <div className='w-fit h-fit p-2 bg-slate-500 text-white rounded-full fixed bottom-[10%] right-[5%]'>
+                <Link to={'/post/new'}>
+                    <TheIcon Icon={FaPlus} size={'40'} />
+                </Link>
+
+            </div>
+            <div>
+                <button
+                    ref={ref}
+                    onClick={() => customPostsQuery.fetchNextPage()}
+                    disabled={!customPostsQuery.hasNextPage || customPostsQuery.isFetchingNextPage}
+                >
+                    {customPostsQuery.isFetchingNextPage ? 'Loading more...'
+                        : customPostsQuery.hasNextPage ? 'Load More'
+                            : !customPostsQuery.isLoading ? 'Nothing more to load' : null}
+                </button>
+            </div>
+
+        </div>
+    </QueryStateWrapper>
 );
 }
