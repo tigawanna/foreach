4e4a819df8a5c35346e071316dbf8551cf35c41d
tigawanna <denniskinuthiaw@gmail.com>
2023-01-14 13:52:57 +0300

    added reaction and reply mutation hooks

--- src/components/replies/RepliesCard.tsx
+++ src/components/replies/RepliesCard.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { CustomRepliesType, PBUser, RepliesType } from '../../utils/types/types';
-import { makeUrl } from './../../utils/pb/config';
+import { client, makeUrl } from './../../utils/pb/config';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { concatErrors } from '../../utils/utils';
+import { Mutationprops } from '../form/types';
+import { Record } from 'pocketbase';
 
 interface ReplyCardProps {
     reply:CustomRepliesType
@@ -46,10 +50,90 @@ return (
 
 
 interface ReplyReactionsCardProps {
-
+    user: PBUser;
+    item: CustomRepliesType;
 }
 
-export const ReplyReactionsCard = ({ }: ReplyReactionsCardProps) => {
+export const ReplyReactionsCard = ({ user,item}: ReplyReactionsCardProps) => {
+
+
+    const [isOpen, setIsOpen] = React.useState(false);
+    const queryClient = useQueryClient();
+    const [liked, setLiked] = React.useState(item.mylike === "yes");
+
+    const updateReactionMutation = useMutation(
+        async (vars: CustomRepliesType) => {
+            const updatevars = { liked: item.mylike === "yes" ? "no" : "yes" };
+            console.log("update mutation vars=== ", updatevars, vars.reaction_id);
+            try {
+                const response = await client.collection("reactions").update(
+                    vars?.reaction_id as string, updatevars);
+                console.log("update reaction response === ", response);
+                return response
+            } catch (err: any) {
+                console.log("error updating ===> ", concatErrors(err));
+                // setError({ name: "main", message: err?.messge })
+                throw err;
+            }
+        },
+        {
+            onSettled: () => {
+                queryClient.invalidateQueries(["custom-posts"]);
+                // queryClient.invalidateQueries(count_query_key);
+            },
+            onError: (err: any) => {
+                console.log("error updating ===> ", concatErrors(err));
+            }
+        }
+    );
+    const newReactionMutation = useMutation(
+        async (vars: CustomRepliesType) => {
+            const newReaction = {
+                post: vars.reply_id,
+                user: user?.id,
+                liked: "yes"
+            };
+            console.log("create vars =====> ", newReaction);
+            try {
+                const response = await client.collection("reactions").create(newReaction);
+                console.log("new reaction response === ", response);
+                return response
+            } catch (err: any) {
+                console.log("error liking post", concatErrors(err));
+                // setError({ name: "main", message: err?.messge })
+                throw err;
+            }
+        },
+        {
+            onSettled: (data) => {
+                queryClient.invalidateQueries(["custom-posts"]);
+                //     queryClient.invalidateQueries(count_query_key);
+            },
+            onError: (err: any) => {
+                console.log("error liking post", concatErrors(err));
+                updateReactionMutation.mutate(item);
+            }
+        }
+    );
+
+    const replyMutation = useMutation(async ({ basepayload }: Mutationprops) => {
+        basepayload.append("depth", (parseInt(item.reply_depth)+1).toString());
+        basepayload.append("post", item.reply_id);
+        // basepayload.append('parent', null)
+        try {
+            return await client.collection("replies").create(basepayload);
+        } catch (e) {
+            throw e;
+        }
+    }, {
+        onSettled: (data: Record | undefined, error: unknown, variables: Mutationprops, context: unknown) => {
+            console.log("data after reply", data)
+            queryClient.invalidateQueries(["custom-posts"]);
+        }
+    }
+
+    );
+
 return (
 <div className="w-full h-full p-2 flex flex-col">
 
