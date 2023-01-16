3a69f52282d580c659fdd7267a9b124d625af2ec
tigawanna <denniskinuthiaw@gmail.com>
2023-01-16 05:52:58 +0300

    fixed nested replies navigation

--- src/components/replies/Replies.tsx
+++ src/components/replies/Replies.tsx
@@ -59,7 +59,16 @@ return (
                     return page.map((item) => {
                         return (
                             <div
-                        // onClick={() => navigate('http://localhost:3000/post/' + item.post_id)}
+                                onClick={() => navigate({
+                                    pathname: '../' + item.post_id,
+                                    search: createSearchParams({
+                                        depth: (item.post_depth === "" ? 0 : item.post_depth).toString()
+                                    }).toString(),
+                                
+
+                                },
+                         
+                                )}
                         key={item.post_id}
                         className="w-[95%]  p-2 flex flex-col  border-black border-2 
                         dark:border-[1px]  dark:border-white rounded-lg gap-3">
