61b16dcc5cd180966fda08be121a73157e6ded8f
tigawanna <denniskinuthiaw@gmail.com>
2023-01-26 20:26:18 +0300

    switched env vars

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -9,11 +9,11 @@ const local_pb = "http://127.0.0.1:8090";
 const web1_url = "https://emps.tigawanna.tech"
 const vercel_url ="https://mech-up-vite.vercel.app";
 
-// export const pb_url=web1_url
-// export const main_url =vercel_url
+export const pb_url=web1_url
+export const main_url =vercel_url
 
-export const pb_url=local_pb
-export const main_url = local_url
+// export const pb_url=local_pb
+// export const main_url = local_url
 
 export const redirect_url=main_url+"/redirect"
 export const login_url = main_url+"/auth";
