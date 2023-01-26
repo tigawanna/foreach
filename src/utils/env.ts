4adcf3b02584cf449965393163eda76d227534ed
tigawanna <denniskinuthiaw@gmail.com>
2023-01-26 11:50:55 +0300

    before master merge

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,10 +5,13 @@ const local_url='http://localhost:3000'
 const local_pb = "http://127.0.0.1:8090";
 const web1_url = "https://emps.tigawanna.tech"
 const vercel_url ="https://mech-up-vite.vercel.app";
+
 // export const pb_url=web1_url
-export const pb_url=local_pb
 // export const main_url =vercel_url
+
+export const pb_url=local_pb
 export const main_url = local_url
+
 export const redirect_url=main_url+"/redirect"
 export const login_url = main_url+"/auth";
 
