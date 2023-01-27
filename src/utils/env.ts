f08e94730e78364fb40ce89b439270adb94d0333
tigawanna <denniskinuthiaw@gmail.com>
2023-01-27 13:07:03 +0300

    fix env typo

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,7 +2,7 @@
 const local_url='http://localhost:3000'
 const local_pb = "http://127.0.0.1:8090";
 
-const web1_url = "https://tigawanna-pocketbase.fly.dev/"
+const web1_url = "https://tigawanna-pocketbase.fly.dev"
 const vercel_url ="https://devhub-brown.vercel.app/";
 
 // export const pb_url=web1_url
