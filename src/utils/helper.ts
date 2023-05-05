7d63ef8b8927cd35c3a3ba998968fc2c9630db4b
tigawanna <denniskinuthiaw@gmail.com>
2023-05-05 23:30:14 +0300

    helpers

---
+++ src/utils/helper.ts
@@ -0,0 +1,9 @@
+export function parseCookie(str){
+    return str
+  .split(';')
+  .map(v => v.split('='))
+  .reduce((acc, v) => {
+      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+      return acc;
+      }, {});    
+}
